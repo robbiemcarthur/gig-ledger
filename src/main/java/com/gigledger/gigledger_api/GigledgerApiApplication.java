e260bd34e99261422df556bad56afb8df9eddb3a
Robbie McArthur <robbiemca123@gmail.com>
2026-02-21 13:44:23 +0000

    chore: initial spring boot project scaffold

---
+++ src/main/java/com/gigledger/gigledger_api/GigledgerApiApplication.java
@@ -0,0 +1,13 @@
+package com.gigledger.gigledger_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GigledgerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GigledgerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gigledger/gigledger_api/GigledgerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigledger.gigledger_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigledgerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
